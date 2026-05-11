e0874e7596d39ee94eb0a3542b2bbcf4332d7e69
Cole <colez3@live.wsd1.org>
2026-05-11 09:56:09 -0500

    Visualizations class added

---
+++ src/Visualizations.java
@@ -0,0 +1,24 @@
+public class Visualizations {
+
+        public static void main(String[] args) {
+
+            String PointsCardVisual = """
+                    ╔══════════════════════════╗
+                    ║      REMPLE RUMBLE       ║
+                    ╠══════════════════════════╣
+                    ║                          ║
+                    ║        Point Card        ║
+                    ║                          ║
+                    ║          ♦ ♦ ♦           ║
+                    ║           ♦ ♦            ║
+                    ║            ♦             ║
+                    ║                          ║
+                    ╠══════════════════════════╣
+                    ║       Gain Points        ║
+                    ╚══════════════════════════╝
+                """;
+
+            System.out.println(PointsCardVisual);
+        }
+    }
+
