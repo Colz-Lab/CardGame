a3b034ba8c3bfb012d005b60328f41c2e296f012
Cole <colez3@live.wsd1.org>
2026-05-11 10:05:54 -0500

    Visualizations for each card have been added.

--- src/Visualizations.java
+++ src/Visualizations.java
@@ -2,7 +2,7 @@ public class Visualizations {
 
         public static void main(String[] args) {
 
-            String PointsCardVisual = """
+            String pointsCardVisual = """
                     ╔══════════════════════════╗
                     ║      REMPLE RUMBLE       ║
                     ╠══════════════════════════╣
@@ -18,7 +18,109 @@ public class Visualizations {
                     ╚══════════════════════════╝
                 """;
 
-            System.out.println(PointsCardVisual);
+            System.out.println(pointsCardVisual);
+
+            String freezeCardVisual = """
+                    ╔══════════════════════════╗
+                    ║      REMPLE RUMBLE       ║
+                    ╠══════════════════════════╣
+                    ║                          ║
+                    ║       Freeze Card        ║
+                    ║                          ║
+                    ║           ♦ ♦            ║
+                    ║       ♦ ♦ ♦ ♦ ♦ ♦        ║
+                    ║           ♦ ♦            ║
+                    ║                          ║
+                    ╠══════════════════════════╣
+                    ║     Freeze A Player      ║
+                    ╚══════════════════════════╝
+                    """;
+            System.out.println(freezeCardVisual);
+
+            String attackCardVisual = """
+                    ╔══════════════════════════╗
+                    ║      REMPLE RUMBLE       ║
+                    ╠══════════════════════════╣
+                    ║                          ║
+                    ║       Attack Card        ║
+                    ║                          ║
+                    ║          ♦ ♦ ♦           ║
+                    ║           ♦ ♦            ║
+                    ║          ♦ ♦ ♦           ║
+                    ║            ♦             ║
+                    ╠══════════════════════════╣
+                    ║     Attack A Player      ║
+                    ╚══════════════════════════╝
+                    """;
+            System.out.println(attackCardVisual);
+
+            String thiefCardVisual = """
+                    ╔══════════════════════════╗
+                    ║      REMPLE RUMBLE       ║
+                    ╠══════════════════════════╣
+                    ║                          ║
+                    ║        Thief Card        ║
+                    ║                          ║
+                    ║          ♦ ♦ ♦           ║
+                    ║       ♦ ♦ ♦ ♦ ♦ ♦        ║
+                    ║           ♦ ♦            ║
+                    ║                          ║
+                    ╠══════════════════════════╣
+                    ║   Steal A Players Card   ║
+                    ╚══════════════════════════╝
+                    """;
+            System.out.println(thiefCardVisual);
+
+            String reverseDirectionCardVisual = """
+                    ╔══════════════════════════╗
+                    ║      REMPLE RUMBLE       ║
+                    ╠══════════════════════════╣
+                    ║                          ║
+                    ║  Reverse Direction Card  ║
+                    ║                          ║
+                    ║      ♦ ♦ ♦ ♦ ♦           ║
+                    ║            ♦             ║
+                    ║          ♦ ♦ ♦ ♦ ♦       ║
+                    ║                          ║
+                    ╠══════════════════════════╣
+                    ║  Reverse Play Direction  ║
+                    ╚══════════════════════════╝
+                    """;
+            System.out.println(reverseDirectionCardVisual);
+
+            String massSwapCardVisual = """
+                    ╔══════════════════════════╗
+                    ║      REMPLE RUMBLE       ║
+                    ╠══════════════════════════╣
+                    ║                          ║
+                    ║      Mass Swap Card      ║
+                    ║                          ║
+                    ║         ♦   ♦   ♦        ║
+                    ║         ♦   ♦   ♦        ║
+                    ║         ♦   ♦   ♦        ║
+                    ║                          ║
+                    ╠══════════════════════════╣
+                    ║   Swap Hands CLockwise   ║
+                    ╚══════════════════════════╝
+                    """;
+            System.out.println(massSwapCardVisual);
+
+            String targetSwapCardVisual = """
+                    ╔══════════════════════════╗
+                    ║      REMPLE RUMBLE       ║
+                    ╠══════════════════════════╣
+                    ║                          ║
+                    ║     Target Swap Card     ║
+                    ║                          ║
+                    ║          ♦    ♦          ║
+                    ║         ♦      ♦         ║
+                    ║        ♦  ♦  ♦  ♦        ║
+                    ║                          ║
+                    ╠══════════════════════════╣
+                    ║ Swap Hands With a Player ║
+                    ╚══════════════════════════╝
+                    """;
+            System.out.println(targetSwapCardVisual);
         }
     }
 
